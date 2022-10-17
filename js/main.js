f6cfb4cc2e0da40454dceb94b249f61c4c67953d
Bushra <77388684+bushra1997@users.noreply.github.com>
2022-10-17 14:57:29 +0300

    frontend task

---
+++ js/main.js
@@ -0,0 +1,26 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+    0: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+          },
+        640: {
+          slidesPerView: 1,
+          spaceBetween: 20,
+        },
+        768: {
+          slidesPerView: 2,
+          spaceBetween: 40,
+        },
+        1024: {
+          slidesPerView: 3,
+          spaceBetween: 50,
+        },
+      },
+  });
